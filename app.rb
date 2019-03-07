40e2cec2d90560f975b7b9868791ba972a9cba42
karamalesa <karamalesa@gmail.com>
2019-03-07 22:58:10 +0200

    save post on db test.sqlite3

--- app.rb
+++ app.rb
@@ -4,6 +4,36 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def get_db
+   @db =  SQLite3::Database.new 'test.sqlite3'
+   @db.results_as_hash = true
+  return @db
+end
+
+def save_form_data_to_database
+  @db = get_db
+  @db.execute 'INSERT INTO post (content)
+  VALUES (?)', [@content]
+  @db.close
+end
+
+before do
+  @db = get_db
+  
+end
+
+configure do
+	@db = get_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS
+	"post"
+	  (
+		"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+		"content"	TEXT
+		
+	  )'
+	  
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
@@ -15,10 +45,10 @@ end
 post '/new' do
 	@content = params[:content]
 
-	f = File.open './public/text.txt', 'a'
-	f.write "Post: #{@content}\n"
-	f.close
-	erb :new
+
+    save_form_data_to_database
+	erb "<h4>Ваша статья отправлена!</h4>"
+		
 
 
 end	
