06cf5ad230c6b8f48552d98eb4b0ee5acaae7184
karamalesa <karamalesa@gmail.com>
2019-03-07 22:22:02 +0200

    add file text.txt on public and add post method on app.rb

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -11,3 +12,13 @@ get '/new' do
 	erb :new
 end	
 
+post '/new' do
+	@textarea = params[:textarea]
+
+	f = File.open './public/text.txt', 'a'
+	f.write "Post: #{@textarea}\n"
+	f.close
+	erb :new
+
+
+end	
